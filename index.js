894e893f7d41cb9ff26c241b138eca0c3b9c50c3
Baran <baranali6688@gmail.com>
2023-09-30 10:29:03 +0300

    Just Little Organize

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET POST PUT DELETE');
 
-    next();
-});
 
 mongoose.connect('mongodb+srv://baran:123@cluster0.vzv1quj.mongodb.net/?retryWrites=true&w=majority')
     .then(() => {
