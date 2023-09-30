1bf33273af2c58408065b0caf399434c4d94f99f
Baran <baranali6688@gmail.com>
2023-09-30 10:16:09 +0300

    Changing Database

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     next();
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/tasks')
+mongoose.connect('mongodb+srv://baran:123@cluster0.vzv1quj.mongodb.net/?retryWrites=true&w=majority')
     .then(() => {
         console.log("Connected to MongoDB")
     })
