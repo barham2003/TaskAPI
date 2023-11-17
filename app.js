7b3b98a08eff4042b7eefea2bbe11da924a4d220
Baran <baranali6688@gmail.com>
2023-11-17 17:37:25 +0300

    Another ):

--- app.js
+++ app.js
@@ -28,16 +28,12 @@ app.use(helmet())
 // Limit Requests form Same IP
 // app.use("/", limiter)
 
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "https://tasks-d5t.pages.dev")
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	next()
-})
-
-app.use(express.json({ limit: "10mb" }))
-app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
 	console.log(req.path, req.method)
 	next()
 })
