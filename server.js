4b7b973a4a887f9d43d89c006561ccf7b0cc8fdf
Baran <baranali6688@gmail.com>
2023-11-17 17:31:40 +0300

    Change again cors!!

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
 require("dotenv").config()
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-
-
-process.on("uncaughtException", err=> {
-    console.log(err.name, err.message)
-    process.exit(1)
-});
-const app = require("./app");
+process.on("uncaughtException", err => {
+	console.log(err.name, err.message)
+	process.exit(1)
+})
+const app = require("./app")
 const DB = process.env.DB_URL
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-
-mongoose.connect(DB)
-.then(() => {console.log("Connected to MongoDB")})
-
-
-
-const port =3000
-const server = app.listen(port, ()=> {
-    console.log("Successfully Connected to port", port)
+	res.setHeader("Access-Control-Allow-Origin", "https://tasks-d5t.pages.dev")
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	next()
 })
 
+mongoose.connect(DB).then(() => {
+	console.log("Connected to MongoDB")
+})
 
+const port = 3000
+const server = app.listen(port, () => {
+	console.log("Successfully Connected to port", port)
+})
 
 process.on("unhandledRejection", err => {
-    console.log(err.name, err.message)
-    server.close(()=> {
-        process.exit(1)
-    })})
-      
+	console.log(err.name, err.message)
+	server.close(() => {
+		process.exit(1)
+	})
+})
